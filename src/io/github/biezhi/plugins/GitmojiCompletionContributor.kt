159effa3b54fcfa490ce01af42addcc63c4db958
bluelovers <codelovers@users.sourceforge.net>
2018-06-14 08:50:40 +0800

    瞎改 硬湊 支援以 : + 文字 來列出表情符號 以及 任何位置都能觸發
    瞎改 硬湊 支援以 : + 文字 來列出表情符號 以及 任何位置都能觸發 
    
    完全不懂 java & kj
    剩下的只能靠懂的人改良了

--- src/io/github/biezhi/plugins/GitmojiCompletionContributor.kt
+++ src/io/github/biezhi/plugins/GitmojiCompletionContributor.kt
@@ -22,19 +22,69 @@ class GitmojiCompletionContributor : CompletionContributor() {
                 if (parameters.editor.isOneLineMode) {
                     return
                 }
-                var message = parameters.editor.document.charsSequence.toString()
+
+                /**
+                 * 瞎改
+                 *
+                 * 參考 https://github.com/JetBrains/intellij-community/blob/master/plugins/xpath/xpath-lang/src/org/intellij/lang/xpath/completion/XPathInsertHandler.java
+                 */
+                val editor = parameters.editor
+                val charsSequence = editor.document.charsSequence
+                val caretModel = editor.caretModel
+                var offset = caretModel.offset
+
+                var message = charsSequence.toString().toLowerCase()
+
                 if (message.length < 2) {
                     return
                 }
+
+                val eb = message.indexOf(":") != -1
+                var pos: Int = if (message.length > offset) offset else 0
+
+                var msg2 = message.substring(pos).trim()
+
+                message = message.trim()
 
                 if (message.indexOf("\n") != -1) {
                     val split = message.split("\n")
                     message = split[split.size - 1]
                 }
 
-                mapping.actions.filterIndexed { _, it -> it.toLowerCase().indexOf(message.toLowerCase()) != -1 }
+                if (message.indexOf(" ") != -1) {
+                    val split = message.split(" ")
+                    message = split[0]
+                }
+
+                if (msg2.indexOf("\n") != -1) {
+                    val split = msg2.split("\n")
+                    msg2 = split[split.size - 1]
+                }
+
+                if (msg2.indexOf(" ") != -1) {
+                    val split = msg2.split(" ")
+                    msg2 = split[split.size - 1]
+                }
+
+                if (eb) {
+                    message = message.replace(":", "")
+                    msg2 = msg2.replace(":", "")
+                }
+
+                mapping.actions
+                        //.filterIndexed { _, it -> it.toLowerCase().indexOf(message.toLowerCase()) != -1 }
                         .forEach {
-                            result.addElement(LookupElementBuilder.create(":${mapping.getText(it)}: " + it).withIcon(mapping.getIcon(it)))
+
+                            val ej = mapping.getText(it) as String
+                            val icon = mapping.getIcon(it)
+
+                            if (eb && (ej.toLowerCase().indexOf(message) != -1 || ej.toLowerCase().indexOf(msg2) != -1)) {
+                                result.addElement(LookupElementBuilder.create(":${ej}:").withIcon(icon))
+                            }
+
+                            if (it.toLowerCase().indexOf(message) != -1 || it.toLowerCase().indexOf(msg2) != -1) {
+                                result.addElement(LookupElementBuilder.create(":${ej}: " + it).withIcon(icon))
+                            }
                         }
             }
         })
